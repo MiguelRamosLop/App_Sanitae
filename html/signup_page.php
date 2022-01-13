a35b65e4352fae8d199e1a6f01c02fdca43bb668
Miguel Ramos López <miguelon2001b@gmail.com>
2022-01-13 13:25:33 +0100

    signup errors js

--- html/signup_page.php
+++ html/signup_page.php
@@ -61,10 +61,10 @@
         <div class="signup_bg_container">
             <div class="center">
                 <h1> Bienvenido a Sanitae </h1>
-                <form method="post" action="home_page.php">
+                <form method="post" action="home_page.php" id="basic-form">
                     <div class="text_field_nom_ape">
                         <input type="text" name="nombre" placeholder="Nombre">
-                        <input type="password" name="apellidos" placeholder="Apellidos">
+                        <input type="text" name="apellidos" placeholder="Apellidos">
                     </div>
                     <div class="text_field">
                         <input type="text" name="username" placeholder="Nombre de usuario">
@@ -73,10 +73,10 @@
                         <input type="text" name="telefono" placeholder="Teléfono">
                     </div>
                     <div class="text_field">
-                        <input type="password" name="password" placeholder="Contraseña">
+                        <input type="password" name="password" placeholder="Contraseña" id="password">
                     </div>
                     <div class="text_field">
-                        <input type="password" name="password" placeholder="Repite contraseña">
+                        <input type="password" name="repite_password" placeholder="Repite contraseña" id="repite_password">
                     </div>
                     <div class="text_field">
                         <input type="text" name="nif" placeholder="NIF">
@@ -94,18 +94,18 @@
                             <input type="radio" name="radio">
                             <span class="checkmark"></span>
                         </label>
+                        <br>
                     </div>
                     <div class="text_field">
-                        <button type="submit" name="login" class="default_btn"> Continuar </button>
-                    </div>
-                    <div class="text_field">
-                        <a href="#" class="condiciones"> Revisar terminos y condiciones </a>
+                        <button type="submit" name="login" class="default_btn"> Registrarme &#8594; </button>
                     </div>
                 </form>
             </div>
             <div class="push"> </div>
         </div>
         <?php include 'footer.html' ?>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+          <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.2/dist/jquery.validate.min.js" crossorigin="anonymous"></script>
         <script type="text/javascript">
             const toggleBtn = document.querySelector('.toggle_button');
 
@@ -113,6 +113,76 @@
 
             toggleBtn.addEventListener('click', function() {
                 navbar_links.classList.toggle('active');
+            });
+
+            $(document).ready(function() {
+                $("#basic-form").validate({
+                    rules: {
+                        nombre: {
+                            required: true
+                        },
+                        apellidos: {
+                            required: true
+                        },
+                        username: {
+                            required: true
+                        },
+                        telefono: {
+                            required: true,
+                            minlength: 9,
+                            maxlength: 9
+                        },
+                        password: {
+                            required: true,
+                            minlength: 5,
+                        },
+                        repite_password: {
+                            required: true,
+                            equalTo: "#password"
+                        },
+                        nif: {
+                            required: true,
+                            minlength: 9
+                        },
+                        nacimiento: {
+                            required: true,
+                            minlength: 8,
+                            maxlength: 10
+                        }
+                    },
+                    messages: {
+                        nombre: {
+                            required: "Campo requerido"
+                        },
+                        apellidos: {
+                            required: "Campo requerido"
+                        },
+                        username: {
+                            required: "Campo requerido"
+                        },
+                        telefono: {
+                            required: "Campo requerido",
+                            minlength: "Proporciona un teléfono en formato válido",
+                            maxlength: "Proporciona un teléfono en formato válido"
+                        },
+                        password: {
+                            required: "Campo requerido",
+                            minlength: "Tu contraseña debe tener al menos 5 caracteres"
+                        },
+                        repite_password: {
+                            required: "Campo requerido",
+                            equalTo: "Las contraseñas no coinciden"
+                        }, 
+                        nif: {
+                            required: "Campo requerido",
+                            minlength: "Proporciona un NIF válido"
+                        },
+                        nacimiento: {
+                            required: "Campo requerido",
+                            minlength: "Proporciona una fecha de nacimiento en el formato especificado"
+                        }
+                    }
+                });
             });
         </script>
     </body>
